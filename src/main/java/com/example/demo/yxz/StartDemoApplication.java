f119097c44eb7c7c824563ae1e71d39579916db8
yxz <first.abyss.walker@gmail.com>
2022-07-04 12:14:47 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/yxz/StartDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo.yxz;
+
+
+import com.example.demo.DemoApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class StartDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
