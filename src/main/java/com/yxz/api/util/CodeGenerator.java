8c0e36ee3809295353f53231ca24581b10027a59
yxz <first.abyss.walker@gmail.com>
2022-07-04 16:12:50 +0800

    添加了代码生成器（需要修改）

--- src/main/java/com/yxz/api/util/CodeGenerator.java
+++ src/main/java/com/yxz/api/util/CodeGenerator.java
@@ -1,11 +1,17 @@
 package com.yxz.api.util;
 
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class CodeGenerator {
 
-    public void  run(){
-        FastAutoGenerator.create("jdbc:mysql://localhost:3306/test", "root", "123456")
+    public static void  run(String table){
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/myproject", "root", "43439494")
                 .globalConfig(builder -> {
                     builder.author("yxz")
                             .enableSwagger()
@@ -13,14 +19,29 @@ public class CodeGenerator {
                     })
                 .packageConfig(builder -> {
                     builder.parent("com.yxz")
-                            .moduleName("");
+                            .moduleName("api")
+                            .entity("entity")
+                            .service("service")
+                            .controller("controller")
+                            .mapper("mapper")
+                            .xml("mapper")
+                            .pathInfo(Collections.singletonMap(OutputFile.xml,System.getProperty("user.dir")+"\\src\\main\\java"))
+                    ;
                 })
+                .strategyConfig(builder -> {
+                    builder.addInclude(table) // 设置需要生成的表名
+                            .addTablePrefix("t_", "c_")
+                            .serviceBuilder(); // 设置过滤表前缀
+                })
+                .templateEngine(new FreemarkerTemplateEngine())
+                .execute();
         ;
     }
 
 
     public static void main(String[] args) {
-
+        String table="User";
+        run(table);
     }
 
 }
